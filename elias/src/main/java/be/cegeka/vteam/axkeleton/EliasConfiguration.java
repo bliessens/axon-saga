fd1409cdfdb387cddc2536275b1f5eddf366c411
bliessens <benoni29@gmail.com>
2018-03-26 11:03:23 +0200

    starting point

---
+++ elias/src/main/java/be/cegeka/vteam/axkeleton/EliasConfiguration.java
@@ -0,0 +1,10 @@
+package be.cegeka.vteam.axkeleton;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("elias")
+public class EliasConfiguration {
+
+}
